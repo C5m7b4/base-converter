3e9f2dcb226ac6d332ee7b973eb16059e034bcaf
C5m7b4 <mbedingfield@dcrpos.com>
2021-12-05 11:53:16 -0600

    add typedoc to isInteger

--- src/utils/is-inteeger.ts
+++ src/utils/is-inteeger.ts
@@ -1,3 +1,8 @@
+/**
+ * this function determines if the input is actually a number
+ * @param number
+ * @returns
+ */
 export const isInteger = (number: number): boolean => {
   return !(typeof number !== 'number' || number % 1);
 };
