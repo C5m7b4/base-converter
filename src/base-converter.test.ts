7ebe2066c3d413a9832c49a0c3227c5e124237b7
C5m7b4 <mbedingfield@dcrpos.com>
2021-12-04 18:12:09 -0600

    add convertFromDecimaltoBase

--- src/base-converter.test.ts
+++ src/base-converter.test.ts
@@ -13,4 +13,24 @@ describe('convertFromBaseNToDecimal', () => {
       expect(converter.convertFromBaseNToDecimal('1210', 3)).toEqual(48);
     });
   });
+  describe('When given a number in a base that is greater than 10', () => {
+    test('should convert it to decimal', () => {
+      expect(converter.convertFromBaseNToDecimal('1A1', 11)).toEqual(232);
+      expect(converter.convertFromBaseNToDecimal('1B1', 14)).toEqual(351);
+      expect(converter.convertFromBaseNToDecimal('ABC', 16)).toEqual(2748);
+    });
+  });
+});
+
+describe('convertFromDecimaltoBaseN', () => {
+  describe('when given a decimal number', () => {
+    test('should convert it to a base N number', () => {
+      expect(converter.convertFromDecimaltoBaseN(13, 2)).toEqual('1101');
+      expect(converter.convertFromDecimaltoBaseN(27, 2)).toEqual('11011');
+      expect(converter.convertFromDecimaltoBaseN(48, 3)).toEqual('1210');
+      expect(converter.convertFromDecimaltoBaseN(232, 11)).toEqual('1A1');
+      expect(converter.convertFromDecimaltoBaseN(351, 14)).toEqual('1B1');
+      expect(converter.convertFromDecimaltoBaseN(2748, 16)).toEqual('ABC');
+    });
+  });
 });
--- src/base-converter.ts
+++ src/base-converter.ts
@@ -1,13 +1,58 @@
+import { lettersToNumbersMap } from './constants/letters-to-numbers-map';
+import { inverseObject } from './utils/inverseObject';
+
 export class BaseConverter {
+  private numbersToLettersMap;
+  constructor() {
+    this.numbersToLettersMap = inverseObject(lettersToNumbersMap);
+  }
   convertFromBaseNToDecimal(numberToConvert: string, baseFrom: number): number {
     // 1101 (binary base2) => 2^3 * 1 + 2^2 * 1 + 2^0 * 1 = 8 + 4 + 1 = 13
     const digitsArray = Array.from(numberToConvert);
     let position = digitsArray.length - 1;
     let result = 0;
     for (const digit of digitsArray) {
-      result += Math.pow(baseFrom, position) * parseInt(digit);
+      result += Math.pow(baseFrom, position) * this.getDigitAsNumber(digit);
       position--;
     }
+    return result;
+  }
+
+  private getDigitAsNumber(digit: string) {
+    let result = parseInt(digit);
+    if (isNaN(result)) {
+      result = lettersToNumbersMap[digit];
+    }
+    return result;
+  }
+
+  convertFromDecimaltoBaseN(numberToConvert: number, baseTo: number): string {
+    let numberToConvertCopy = numberToConvert;
+    const remainders = [];
+    while (true) {
+      const quotient = Math.floor(numberToConvertCopy / baseTo);
+      const remainder = numberToConvertCopy % baseTo;
+      remainders.push(
+        remainder >= 10
+          ? this.numbersToLettersMap[remainder]
+          : remainder.toString()
+      );
+      numberToConvertCopy = quotient;
+      if (numberToConvertCopy < baseTo) {
+        break;
+      }
+    }
+
+    remainders.reverse();
+    let result =
+      numberToConvertCopy >= 10
+        ? (this.numbersToLettersMap[numberToConvertCopy] as string)
+        : numberToConvertCopy.toString();
+
+    remainders.forEach((remainder) => {
+      result += remainder;
+    });
+
     return result;
   }
 }
